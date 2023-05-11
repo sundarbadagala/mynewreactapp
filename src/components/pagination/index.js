e936972f108cdd33576beec0ce8bfd7954b12da8
sundar <sundar.badagala@gmail.com>
2023-05-11 08:18:33 +0530

    pagination code update

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
-import { usePagination } from '../../hooks/usePagination'
 import Pagination from './pagination'
 import './styles.css'
 
